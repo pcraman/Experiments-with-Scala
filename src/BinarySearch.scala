1d3f68e54f50b6bd023c4094381e2fd87147b75c
Poornima Chozhiyath Raman <poornima.craman@gmail.com>
2016-08-02 23:09:46 -0700

    BinarySearch sample code

---
+++ src/BinarySearch.scala
@@ -0,0 +1,23 @@
+
+object BinarySearch {
+	def main(args: Array[String]) {
+		val l = List(2,4,5);
+		val res = binSearch(l, 4);
+		println ("\nAnswer = " + res);
+	}
+	
+	def binSearch(l:List[Int], target:Int):Int ={
+		var left = 0;
+		var right = l.size - 1;
+		while (left <= right) {
+			val mid = (left + right)/2;
+			if (l(mid) == target)
+				return mid + 1;
+			else if (l(mid) > target)
+				right = mid - 1;
+			else 
+				left = mid + 1;
+		}
+		return -1;
+	}
+}
