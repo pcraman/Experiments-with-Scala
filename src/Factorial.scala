c12f8d3727c72e70c6e3e19f93b1a9fb865093e8
Poornima Chozhiyath Raman <poornima.craman@gmail.com>
2016-08-09 23:17:30 -0700

    Factorial Iterative

---
+++ src/Factorial.scala
@@ -0,0 +1,17 @@
+
+object Factorial {
+	def main(args : Array[String]) {
+		var res = factorialNum(5)
+		println ("Answer = " + res)
+	}
+
+	def factorialNum (n : Int) : Int ={
+		var i = n
+		var res = 1
+		while (i > 0) {
+			res = res * i
+			i = i - 1
+		}
+		return res
+	}
+}
