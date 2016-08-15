d8956b97e4d1533e806555e47c38bc57378a15b6
Poornima Chozhiyath Raman <poornima.craman@gmail.com>
2016-08-14 23:18:54 -0700

    Start programs with strings

---
+++ src/strings/Anagram.scala
@@ -0,0 +1,16 @@
+// Using library functions
+object Anagram {
+	def main(args : Array[String]) {
+		val x = "cat"
+		val y = "act"
+		println (s"Answer - $x and $y are " + checkAnagram(x, y))
+
+	}
+
+	def checkAnagram (x : String, y : String) : String ={
+		if ( (x diff y).isEmpty) 
+			return "Anagrams"
+		else 
+			return "not Anagrams"
+	} 
+}
