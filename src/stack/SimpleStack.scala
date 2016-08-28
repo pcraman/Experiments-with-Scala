f194695b1115bf43adefcdab25faf88e204ddd54
Poornima Chozhiyath Raman <poornima.craman@gmail.com>
2016-08-27 21:43:40 -0700

    SimpleStack program

--- src/stack/SimpleStack.scala
+++ src/stack/SimpleStack.scala
@@ -1,9 +1,17 @@
 // This is a simple program to show how to use stacks in scala
+import scala.collection.mutable.Stack;
 object SimpleStack {
   def main(args: Array[String]) {
-
+    val str = "helloworld"
+    val res = reverseStringWithStack(str)
+    println(s"The reversal of ${str} is = ${res}")
   }
   def reverseStringWithStack(str: String): String = {
-
+    var letters = Stack[Char]()
+    str.map(letters.push(_))
+    var reversedString :String = ""
+    while (!letters.isEmpty)
+      reversedString += letters.pop
+    return reversedString
   }
 }
