c19beb49d4ce51fcccbfa4f5bb92d63aa1d4fb84
Poornima Chozhiyath Raman <poornima.craman@gmail.com>
2016-09-06 22:45:17 -0700

    Preorder and postorder traversal

--- src/tree/SimpleBinaryTree.scala
+++ src/tree/SimpleBinaryTree.scala
@@ -12,9 +12,34 @@ object SimpleBinaryTree {
         if (x.right != None) inorderTraversal(x.right)
     }
   }
+
+  def preorderTraversal(t: Option[Tree[Int]]): Unit = {
+    t match {
+      case None =>
+      case Some(x) =>
+        println(x.value)
+        if (x.left != None) preorderTraversal(x.left)
+        if (x.right != None) preorderTraversal(x.right)
+    }
+  }
+
+  def postorderTraversal(t: Option[Tree[Int]]): Unit = {
+    t match {
+      case None =>
+      case Some(x) =>
+        if (x.left != None) postorderTraversal(x.left)
+        if (x.right != None) postorderTraversal(x.right)
+        println(x.value)
+    }
+  }
   def main(arr: Array[String]) {
     var t = Some(Tree(7, Some(Tree(3, None, None)), Some(Tree(2, None, None))))
-    println(s"Inorder Traversal of tree : ${t} is = ")
+    println(s"Tree : ${t} ")
+    println("Inorder Traversal = ")
     inorderTraversal(t)
+    println("Preorder Traversal = ")
+    preorderTraversal(t)
+    println("Postorder Traversal = ")
+    postorderTraversal(t)
   }
 }
