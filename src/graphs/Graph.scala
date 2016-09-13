97debd395f274f23fe853ecd31c4a87d52164f90
Poornima Chozhiyath Raman <poornima.craman@gmail.com>
2016-09-12 23:21:38 -0700

    Modifications

--- src/graphs/Graph.scala
+++ src/graphs/Graph.scala
@@ -1,23 +1,26 @@
 
-case class Vertex(v : Int)
-case class Edge(v1 : Vertex, v2 : Vertex, w : Int = 0)
+case class Edge(v1 : Int, v2 : Int, w : Int = 0)
 
-class Graph (var numOfVertices: Int){
-
-  var vertices = Set[Vertex]()
+class Graph (var n: Int){
+  val numOfVertices = n
+  var vertices = Set[Int]()
   var edges = List[Edge]()
-  var adjacencyList = Map[Vertex, List[Vertex]]()
-  (1 to numOfVertices).toList.map(vertices += Vertex(_))
+  var adjacencyList = Map[Int, List[Int]]()
 
-  def addEdge(v1 : Vertex, v2 : Vertex, isDirected : Boolean = false) = {
-    println("Adding the edge")
-    val e1 = new Edge(v1, v2)
-    adjacencyList += e1.v1 -> (e1.v2::adjacencyList.getOrElse(e1.v1, Nil))
-    if (!isDirected) {
-      val e2 = new Edge(v2, v1)
-      adjacencyList += e2.v1 -> (e2.v2::adjacencyList.getOrElse(e1.v1, Nil))
+  def addEdge(v1 : Int, v2 : Int, isDirected : Boolean = false) = {
+    if((!vertices.contains(v1) || !vertices.contains(v2))
+        && vertices.size == numOfVertices)
+      println("Cannot Add vertices, ERROR!")
+    else {
+      vertices += v1
+      vertices += v2
+      println("Adding the edge " + v1 + " " + v2)
+      val e1 = new Edge(v1, v2)
+      adjacencyList += e1.v1 -> (e1.v2::adjacencyList.getOrElse(e1.v1, Nil))
+      if (!isDirected) {
+        val e2 = new Edge(v2, v1)
+        adjacencyList += e2.v1 -> (e2.v2::adjacencyList.getOrElse(e1.v1, Nil))
+      }
     }
   }
-
-  
 }
--- src/graphs/SimpleGraph.scala
+++ src/graphs/SimpleGraph.scala
@@ -3,7 +3,10 @@ object SimpleGraph {
 
   def main(args : Array[String]) {
     var g = new Graph(4)
-    g.addEdge(Vertex(1), Vertex(3))
-    println("Size is" + g.vertices.size)
+    g.addEdge(1, 3)
+    g.addEdge(1, 4)
+    g.addEdge(2, 3)
+    g.addEdge(1, 5)
+    println("Size is " + g.vertices.size)
   }
 }
